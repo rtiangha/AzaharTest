63057c87b51289f0c9f13feee984c0cc6c95af40
Reg Tiangha <rtiangha@users.noreply.github.com>
2026-07-07 17:31:34 -0600

    Update various Android dependencies

--- src/android/build.gradle.kts
+++ src/android/build.gradle.kts
@@ -6,8 +6,8 @@
 plugins {
     id("com.android.application") version "8.13.2" apply false
     id("com.android.library") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
 }
 
 tasks.register("clean").configure {
@@ -19,6 +19,6 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.0")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.8")
     }
 }

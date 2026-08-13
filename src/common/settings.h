29a2b89ff37fcc2d37bfb521c0a3476cc6c9fe00
72374 <250991390+72374@users.noreply.github.com>
2026-08-13 14:52:16 +0200

    feat: Offer settings for up to 324 times the original resolution ("18x") (#1906)

--- src/common/settings.h
+++ src/common/settings.h
@@ -544,7 +544,7 @@ struct Values {
     SwitchableSetting<bool> use_display_refresh_rate_detection{
         true, Keys::use_display_refresh_rate_detection};
     Setting<bool> use_shader_jit{true, Keys::use_shader_jit};
-    SwitchableSetting<u32, true> resolution_factor{1, 0, 10, Keys::resolution_factor};
+    SwitchableSetting<u32, true> resolution_factor{1, 0, 18, Keys::resolution_factor};
     SwitchableSetting<bool> use_integer_scaling{false, Keys::use_integer_scaling};
     SwitchableSetting<double, true> frame_limit{100, 0, 1000, Keys::frame_limit};
     SwitchableSetting<double, true> turbo_limit{200, 0, 1000, Keys::turbo_limit};

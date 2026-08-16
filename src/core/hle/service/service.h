054826ded1c1f98426a3e32dae2bf3a9ee514aee
PabloMK7 <hackyglitch2@gmail.com>
2026-08-16 21:04:05 +0200

    core: Fix another msvc compiler bug

--- src/core/hle/service/service.h
+++ src/core/hle/service/service.h
@@ -132,7 +132,20 @@ protected:
          *     the request
          * @param name human-friendly name for the request. Used mostly for logging purposes.
          */
-        constexpr FunctionInfo(u32 command_id, HandlerFnP<Self> handler_callback, const char* name)
+#ifdef _MSC_VER
+        // This is the ifdef of shame.
+        // Microslop is apparenly unable to have a properly working compiler, so we need to disable
+        // constexpr evaulation of the FunctionInfo constructor.
+        //
+        // When pointer-to-member callbacks are passed through a constexpr, it can cause the
+        // resulting array to have elements of different size:
+        // https://developercommunity.visualstudio.com/t/Incorrect-struct-array-memory-layout/11011322
+        // or to have invalid `this` delta information:
+        // https://developercommunity.visualstudio.com/t/MSVC-vmg-sets-invalid-pointer-to-member/11136987
+#else
+        constexpr
+#endif
+        FunctionInfo(u32 command_id, HandlerFnP<Self> handler_callback, const char* name)
             : FunctionInfoBase{
                   command_id,
                   // Type-erase member function pointer by casting it down to the base class.

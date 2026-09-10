ad1fda25fadba78193ac5eb6eef885826ff0edc5
Wunk <wunkolo@gmail.com>
2026-09-10 07:53:18 -0700

    renderer_vulkan: Allow instance creation in systems with no layers. (#2528)

--- src/video_core/renderer_vulkan/vk_platform.cpp
+++ src/video_core/renderer_vulkan/vk_platform.cpp
@@ -329,6 +329,10 @@ vk::UniqueInstance CreateInstance(const Common::DynamicLibrary& library,
 
     // Sanitize layers list
     const auto layer_properties = vk::enumerateInstanceLayerProperties();
+    if (layer_properties.empty()) {
+        LOG_WARNING(Render_Vulkan, "Instance layer properties list is empty");
+    }
+
     boost::container::erase_if(layers, [&](const char* layer) -> bool {
         const auto it = std::find_if(
             layer_properties.begin(), layer_properties.end(),

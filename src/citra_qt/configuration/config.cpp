4fe5bae1e893a4f1545ea96cf1c37e58c253d819
OpenSauce04 <opensauce04@gmail.com>
2026-09-13 15:36:56 +0100

    qt: Add fastinterp toggle setting to frontend

--- src/citra_qt/configuration/config.cpp
+++ src/citra_qt/configuration/config.cpp
@@ -488,6 +488,7 @@ void QtConfig::ReadCoreValues() {
 
     if (global) {
         ReadBasicSetting(Settings::values.use_cpu_jit);
+        ReadBasicSetting(Settings::values.use_fastinterp);
         ReadBasicSetting(Settings::values.delay_start_for_lle_modules);
     }
 
@@ -1086,6 +1087,7 @@ void QtConfig::SaveCoreValues() {
 
     if (global) {
         WriteBasicSetting(Settings::values.use_cpu_jit);
+        WriteBasicSetting(Settings::values.use_fastinterp);
         WriteBasicSetting(Settings::values.delay_start_for_lle_modules);
     }
 
